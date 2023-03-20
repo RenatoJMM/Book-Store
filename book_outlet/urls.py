682e093ed73441a1f13afecba2dfc2a5e7224e1b
Renato Magalhaes <renato.jm@hotmail.com>
2023-03-19 21:17:27 -0300

    Adding header

--- book_outlet/urls.py
+++ book_outlet/urls.py
@@ -1,6 +1,6 @@
 from django.urls import path
 from . import views
 urlpatterns = [
-    path("", views.index),
+    path("", views.index, name="index"),
     path("<slug:slug>", views.book_detail_slug, name="book-detail-page")
 ]
--- book_outlet/views.py
+++ book_outlet/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render, get_object_or_404
 from .models import Book
-from django.http import Http404,JsonResponse
+from django.http import Http404
 from django.db.models import Avg, Max, Min
 
 # Create your views here.
@@ -26,7 +26,6 @@ def index(request):
 
 
 def book_detail_slug(request, slug):
-
     book = get_object_or_404(Book, slug=slug)
 
     return render(request, "book_outlet/book_detail.html", {
