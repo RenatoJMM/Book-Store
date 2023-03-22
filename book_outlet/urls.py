5492655b7fbb1c67557fa6699f1da5a02d8bee04
Renato Magalhaes <renato.jm@hotmail.com>
2023-03-21 23:21:56 -0300

    Fixing thank-you page and creating all_books page

--- book_outlet/urls.py
+++ book_outlet/urls.py
@@ -3,6 +3,7 @@ from . import views
 
 urlpatterns = [
     path("", views.IndexView.as_view(), name="index"),
-    path("<slug:slug>", views.book_detail_slug, name="book-detail-page"),
-    path("thank-you", views.ThankYouView.as_view())
+    path("thank-you", views.ThankYouView.as_view()),
+    path("all-books", views.AllBooksView.as_view(), name="all-books"),
+    path("<slug:slug>", views.book_detail_slug, name="book-detail-page")
 ]
--- book_outlet/views.py
+++ book_outlet/views.py
@@ -5,6 +5,7 @@ from django.db.models import Avg, Max, Min
 
 from django.views import View
 from django.views.generic.base import TemplateView
+from django.views.generic import ListView
 from .forms import BookForm
 
 # Create your views here.
@@ -15,7 +16,7 @@ class IndexView(View):
 
         books = Book.objects.all().order_by("-title")
         num_books = books.count()
-        average_rating = books.aggregate(Avg("rating"), Min("rating"))
+        statistics = books.aggregate(Avg("rating"), Min("rating"))
 
         form = BookForm()
 
@@ -23,17 +24,24 @@ class IndexView(View):
             "form": form,
             "books": books,
             "num_books": num_books,
-            "average_rating": average_rating
+            "statistics": statistics
         })
 
     def post(self, request):
         form = BookForm(request.POST)
+        books = Book.objects.all().order_by("-title")
+        num_books = books.count()
+        statistics = books.aggregate(Avg("rating"), Min("rating"))
+
         if form.is_valid():
             form.save()
             return HttpResponseRedirect("/thank-you")
 
         return render(request, "book_outlet/index.html", {
-            "form": form
+            "form": form,
+            "books": books,
+            "num_books": num_books,
+            "statistics": statistics
         })
 
 
@@ -56,6 +64,20 @@ class IndexView(View):
     """
 
 
+class AllBooksView(ListView):
+    template_name = "book_outlet/all_books.html"
+    model = Book
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        books = Book.objects.all()
+        context['books'] = books
+        context['num_books'] = books.count()
+        context['statistics'] = books.aggregate(Avg("rating"), Min("rating"))
+
+        return context
+
+
 def book_detail_slug(request, slug):
     book = get_object_or_404(Book, slug=slug)
 
@@ -69,4 +91,4 @@ def book_detail_slug(request, slug):
 
 
 class ThankYouView(TemplateView):
-    template = "book_outlet/thank-you"
+    template_name = "book_outlet/thank-you.html"
